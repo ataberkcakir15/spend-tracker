9912c1da8fbaacf2f624c063aba0bd9b0588cb31
ataberkcakir15 <157164103+ataberkcakir15@users.noreply.github.com>
2024-08-23 10:51:23 +0300

    Files

---
+++ SpendTracker/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendTracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+
+        [Required] // Description girmek zorunlu anlamina geliyor
+        public string? Description { get; set; }
+    }
+}
---
+++ SpendTracker/Models/SpendTrackerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendTracker.Models
+{
+    public class SpendTrackerDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public SpendTrackerDbContext(DbContextOptions<SpendTrackerDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Expense> Expenes {  get; set; }
+    }
+}
